4c1bf0b4e78c195806f4abd01b7b5ddb86025738
jeanmariegomis <gomisj170@gmail.com>
2019-07-29 14:09:04 +0000

    depot

--- src/Controller/TransactionController.php
+++ src/Controller/TransactionController.php
@@ -4,6 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use App\Entity\Depot;
 
 class TransactionController extends AbstractController
 {
@@ -16,4 +20,28 @@ class TransactionController extends AbstractController
             'controller_name' => 'TransactionController',
         ]);
     }
+
+    public function depot (Request $request)
+    {
+        $depot = new Depot();
+
+        $form = $this->createForm(Depottype::class,$depot);
+        $data=json_decode($request->getContent(),true);
+        $form->submit($data);
+
+        if($form->isSubmitted())
+        {
+            $dep=$this->getDoctrine()->getManager();
+            $dep->persist($depot);
+            $dep->flush();
+            return $this->handleView($this->view(['status' => 'ok'], Response::HTTP_CREATED));
+
+        }
+
+        return $this->handleView($this->view($form->getErrors));
+
+        
+    }
 }
+
+
